431106dd8b670becc1b6e0116c7f0a7a3877c25e
Kirill <Petrovich_1811@mail.ru>
2016-06-22 23:52:32 +0300

    Дописана фабрика

--- include/abstract_fabrican.h
+++ include/abstract_fabrican.h
@@ -5,50 +5,60 @@
 #include <iostream>
 #include <memory>
 
+/* АБСТРАКТНЫЕ ПРОДУКТЫ */
+
 // aбстрактный продукт - предметы сиденья
 class Chair {
 public:
-  virtual Chair* clone() = 0;  // Метод клонирование сиденья
-  virtual void draw()    = 0;  // Отрисовка сиденья
+  virtual Chair* clone()    = 0;  // Метод клонирование сиденья
+  virtual void   draw()     = 0;  // Отрисовка сиденья
 };
-
 
 // абстрактный продукт - предметы чистки
 class Cleaning {
 public:
-  virtual Cleaning* clone() = 0;  // Метод клонирование чистящих предметов
-  virtual void draw()    = 0;  // Отрисовка чистящих предметов
+  virtual Cleaning* clone()  = 0;  // Метод клонирование чистящих предметов
+  virtual void      draw()   = 0;  // Отрисовка чистящих предметов
 };
+
+// абстрактный продукт - разновидные виды тумбочек
+class Stand {
+public:
+  virtual Stand* clone()     = 0;  // Метод клонирование тумб
+  virtual void   draw()      = 0;  // Отрисовка тумб
+};
+
+/* КОНКРЕТНЫЕ ПРОДУКТЫ */
 
 // конкретный продукт сидения для кухни (стул)
 class KitchenChair : public Chair {
 public:
-    virtual KitchenChair* clone() override {
+    virtual KitchenChair*     clone() override {
       return new KitchenChair(*this);
     }
-    void draw() override {
+    virtual void              draw()  override {
       std::cout << "Cтул " << std::endl;
     }
 };
 
-// конкретный продукт сидения для ванной (туалет)
+// конкретный продукт сидения для ванной (унитаз)
 class BathroomChair : public Chair{
 public:
-    virtual BathroomChair* clone() override {
+    virtual BathroomChair*    clone() override {
       return new BathroomChair(*this);
     }
-    void draw() override {
-      std::cout << "Туалет " << std::endl;
+    virtual void              draw()  override {
+      std::cout << "Унитаз " << std::endl;
     }
 };
 
 // конкретный продукт чистки для кухни (посудомойка)
 class KitchenCleaning : public Cleaning {
 public:
-    virtual KitchenCleaning* clone() override {
+    virtual KitchenCleaning*  clone() override {
       return new KitchenCleaning(*this);
     }
-    void draw() override {
+    virtual void              draw()  override {
       std::cout << "Посудомойка " << std::endl;
     }
 };
@@ -59,37 +69,97 @@ public:
     virtual BathroomCleaning* clone() override {
       return new BathroomCleaning(*this);
     }
-    void draw() override {
+    virtual void              draw()  override {
       std::cout << "Душ " << std::endl;
     }
 };
 
-// абстрактная фабрика
-class Abstract {
+// конкретный продук: кухонная тумбочка
+class KitchenStand : public Stand {
 public:
-  virtual Chair* createChair () = 0;
-  virtual Cleaning* createCleaning () = 0;
+    virtual KitchenStand*     clone() override {
+      return new KitchenStand(*this);
+    }
+    virtual void              draw()  override {
+      std::cout << "Стол " << std::endl;
+    }
 };
 
+// конкретный продукт: ванная тумбочка
+class BathroomStand : public Stand {
+public:
+    virtual BathroomStand*    clone() override {
+      return new BathroomStand(*this);
+    }
+    virtual void              draw()  override {
+      std::cout << "Умывальник " << std::endl;
+    }
+};
+
+/* АБСТРАКТНАЯ ФАБРИКА */
+
+class FurnitureFactory {
+public:
+  virtual Chair*    createChair ()     = 0;
+  virtual Stand*    createStand ()     = 0;
+  virtual Cleaning* createCleaning ()  = 0;
+};
+
+/* КОНКРЕТНЫЕ ФАБРИКИ */
 
  // фабрика кухни
-class Kitchen : public Abstract{
+class Kitchen : public FurnitureFactory{
 public:
-  virtual Chair* createChair () override {
+  virtual Chair*     createChair ()    override {
     return new KitchenChair();
   }
-  virtual Cleaning* createCleaning () override {
+  virtual Stand*     createStand ()    override {
+    return new KitchenStand();
+  }
+  virtual Cleaning*  createCleaning () override {
     return new KitchenCleaning();
   }
 };
 
 // фабрика ванной
-class Bathroom : public Abstract{
+class Bathroom : public FurnitureFactory{
 public:
-  virtual Chair* createChair () override {
+  virtual Chair*     createChair ()    override {
     return new BathroomChair();
   }
-  virtual Cleaning* createCleaning () override {
-    return new KitchenCleaning();
+  virtual Stand*     createStand ()    override {
+    return new BathroomStand();
+  }
+  virtual Cleaning*  createCleaning () override {
+    return new BathroomCleaning();
   }
 };
+
+/* КЛИЕНТ */
+
+class Collection {
+public:
+  explicit Collection (FurnitureFactory* fabric) {
+    chair = fabric->createChair();
+    stand = fabric->createStand();
+    cleaning = fabric->createCleaning();
+  }
+  void draw () {
+    chair->draw();
+    stand->draw();
+    cleaning->draw();
+  }
+
+
+  ~Collection() {
+    delete chair;
+    delete stand;
+    delete cleaning;
+  }
+
+protected:
+  Chair* chair;
+  Stand* stand;
+  Cleaning* cleaning;
+};
+
